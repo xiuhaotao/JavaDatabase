e4d7f11b4a30d62819e28a905939468a7ab9828c
Liu Hao <Liu Hao>
2016-12-13 21:08:08 -0500

    fixed bug that add / edit a player info, player selection droplist doesn't update.

--- src/exercise1/view/GameEditWin.java
+++ src/exercise1/view/GameEditWin.java
@@ -2,7 +2,6 @@ package exercise1.view;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
